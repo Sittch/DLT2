d02311f46b687aaedcdb098a4f87e2802e65bb54
PaulMaxHoffmann <p.m.hoffmann@live.com>
2023-02-06 22:16:24 -0700

    Realigned shape

--- model.py
+++ model.py
@@ -92,6 +92,8 @@ X_train, x_test, Y_train, y_test = train_test_split(seqs, labels, test_size=0.3,
 under_sampler = RandomUnderSampler(random_state=42)
 X_train_bal, Y_train_bal = under_sampler.fit_resample(X_train, Y_train)
 x_test_bal, y_test_bal = under_sampler.fit_resample(x_test, y_test)
+print(shape(X_train),shape(Y_train))
+print(shape(x_test), shape(y_test))
 
 #Using Neural Networks
 
@@ -159,7 +161,7 @@ embedded_sequences = embedding_layer(int_sequences_input)
 x = layers.Bidirectional(layers.LSTM(1024, return_sequences=True))(embedded_sequences)
 x = layers.Bidirectional(layers.LSTM(1024))(x)
 # before = layers.Dense(20, activation="relu")(x)
-preds = layers.Dense(9, activation="softmax")(x)
+preds = layers.Dense(8, activation="softmax")(x)
 model = Model(int_sequences_input, preds)
 
 
